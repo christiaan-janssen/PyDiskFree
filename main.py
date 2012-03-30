8385a0806da94ef382af0e986346579b296eb74c
Christiaan Janssen <heli0s@kpnmail.nl>
2012-03-30 13:39:12 +0200

    Added a warning for drives with low space

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from diskFree import Disk
 
 workstations = []
+warnings = []
 
 # Edit this text file to change the server names.
 f = open('../servers.txt', 'r')
@@ -16,4 +17,14 @@ print 'Hard drive sizes:'
 for compName in workstations:
 	for disk in drive:
 		computer = Disk(compName, disk)
-		print 'Freespace on %s\%s: %i GB' % (computer.name, computer.drive,  computer.FreeSpace())
+		print "Freespace on %s\\%s: %i GB" % (computer.name, computer.disk, computer.FreeSpace())
+
+		if computer.FreeSpace() < 150 and computer.disk == "d$":
+			warnings.append(computer)
+
+
+for computer in warnings:
+	print ""
+	print "*** Warning ***"
+	print "Space on these drive(s) is getting low:"
+	print computer.drive
